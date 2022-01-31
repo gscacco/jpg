ddfcc4909c84a017c3fedd54c1a0656fb903ced7
Gianluca Scacco <gianluca.scacco@gmail.com>
2022-01-31 21:51:46 +0100

    Read start of the image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::Read;
+fn main() -> std::io::Result<()> {
+    let fname = std::env::args()
+        .skip(1)
+        .next()
+        .expect("You must provide file name");
+    println!("Analizing <{:?}>", fname);
+    let mut f = File::open(fname)?;
+    let mut buf = [0u8; 2];
+    f.read(&mut buf)?;
+    println!("{:#02X} {:#02X}", buf[0], buf[1]);
+    Ok(())
 }
