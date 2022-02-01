c61fba4d118bbd3ff73ba1ed9cf1cd6c89d29b3c
Gianluca Scacco <gianluca.scacco@gmail.com>
2022-02-01 08:34:02 +0100

    reading app0 marker

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
 use std::fs::File;
 use std::io::Read;
+
+fn read_file(fname: String) -> std::io::Result<()> {
+    let mut f = File::open(fname)?;
+
+    let mut soi = [0u8; 2];
+    f.read_exact(&mut soi)?;
+    assert_eq!([0xFF, 0xD8], soi);
+
+    let mut app0_marker = [0u8; 2];
+    f.read_exact(&mut app0_marker)?;
+    assert_eq!([0xFF, 0xE0], app0_marker);
+
+    Ok(())
+}
 fn main() -> std::io::Result<()> {
     let fname = std::env::args()
         .skip(1)
         .next()
         .expect("You must provide file name");
     println!("Analizing <{:?}>", fname);
-    let mut f = File::open(fname)?;
-    let mut buf = [0u8; 2];
-    f.read(&mut buf)?;
-    println!("{:#02X} {:#02X}", buf[0], buf[1]);
+    read_file(fname)?;
     Ok(())
 }
