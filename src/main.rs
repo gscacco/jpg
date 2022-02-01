f2c055de28211cda5e8a9dcc6cb5acb7173c1b5d
Gianluca Scacco <gianluca.scacco@gmail.com>
2022-02-01 09:31:18 +0100

    read struct directly

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,45 @@
 use std::fs::File;
 use std::io::Read;
 
+#[repr(C, packed)]
+#[derive(Debug, Copy, Clone)]
+struct App0Marker {
+    app0_marker: [u8; 2],
+    length: [u8; 2],
+    identifier: [u8; 5],
+    version: [u8; 2],
+    density_units: u8,
+    xdensity: [u8; 2],
+    ydensity: [u8; 2],
+    xthumbnail: u8,
+    ythumbnail: u8,
+}
+impl App0Marker {
+    fn get_length(&self) -> u16 {
+        (self.length[0] as u16 * 256 + self.length[1] as u16).into()
+    }
+}
+
+#[repr(C, packed)]
+#[derive(Debug, Copy, Clone)]
+struct Header {
+    soi: [u8; 2],
+    app0: App0Marker,
+}
+
 fn read_file(fname: String) -> std::io::Result<()> {
     let mut f = File::open(fname)?;
 
-    let mut soi = [0u8; 2];
-    f.read_exact(&mut soi)?;
-    assert_eq!([0xFF, 0xD8], soi);
+    let mut header: Header = unsafe { std::mem::zeroed() };
+    let header_size = std::mem::size_of::<Header>();
 
-    let mut app0_marker = [0u8; 2];
-    f.read_exact(&mut app0_marker)?;
-    assert_eq!([0xFF, 0xE0], app0_marker);
-
+    unsafe {
+        let config_slice =
+            std::slice::from_raw_parts_mut(&mut header as *mut _ as *mut u8, header_size);
+        f.read_exact(config_slice).unwrap();
+    }
+    println!("Read structure: {:#?}", header);
+    println!("App0 length {}", header.app0.get_length());
     Ok(())
 }
 fn main() -> std::io::Result<()> {
