ed8105cc99bc032b3db07a39da3753fb24fede1f
Gianluca Scacco <gianluca.scacco@gmail.com>
2022-02-01 15:35:52 +0100

    skip thumbnail data

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::fs::File;
-use std::io::Read;
+use std::io::{Read, Seek};
+
+//https://en.wikipedia.org/wiki/JPEG_File_Interchange_Format
 
 fn bigendian16(arr: [u8; 2]) -> u16 {
     (arr[0] as u16 * 256 + arr[1] as u16).into()
@@ -52,6 +54,11 @@ fn read_file(fname: String) -> std::io::Result<()> {
     println!("App0 length {}", header.app0.get_length());
     println!("App0 xdensity {}", header.app0.get_xdensity());
     println!("App0 ydensity {}", header.app0.get_ydensity());
+    let app0_rst = header.app0.get_length() - 16;
+    if app0_rst > 0 {
+        f.seek(std::io::SeekFrom::Current(app0_rst.into()))?;
+    }
+    println!("{}", app0_rst);
     Ok(())
 }
 fn main() -> std::io::Result<()> {
